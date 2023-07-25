eab702a1c485d4331c787f3cd93cc75faf82b333
mac <pro@MacBook-Pro-van-mac.local>
2023-07-25 22:56:29 +0100

    %sc

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+# define MAIN_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <stdarg.h>
+
+int	ft_putchar(char c);
+int	ft_putstr(char *s);
+int	ft_printf(const char *s, ...);
+int	ft_convert(char c, va_list ap);
+
+#endif
